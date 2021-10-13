763a3fa8356c2e68d9183e0f35024d53625517ef
Matei Oltean <matei.oltean@gmail.com>
2021-10-13 11:24:13 +0200

    Add code highlighting and math parsing

---
+++ src/Jupyscope.Client/Helpers/HTMLHeaderHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace Jupyscope.Client.Helpers
+{
+    static class HTMLHeaderHelper
+    {
+        public static readonly string Header = $@"<!DOCTYPE html>{mathFormatterScript}{codeHighlighterScript}";
+
+        private const string mathFormatterScript = "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/katex.min.css\" integrity=\"sha384-zTROYFVGOfTw7JV7KUu8udsvW2fx4lWOsCEDqhBreBwlHI4ioVRtmIvEThzJHGET\" crossorigin=\"anonymous\"><script defer src=\"https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/katex.min.js\" integrity=\"sha384-GxNFqL3r9uRJQhR+47eDxuPoNE7yLftQM8LcxzgS4HT73tp970WS/wV5p8UzCOmb\" crossorigin=\"anonymous\"></script><script defer src=\"https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/contrib/auto-render.min.js\" integrity=\"sha384-vZTG03m+2yp6N6BNi5iM4rW4oIwk5DfcNdFfxkk9ZWpDriOkXX8voJBFrAO7MpVl\" crossorigin=\"anonymous\" onload=\"renderMathInElement(document.body);\"></ script>";
+
+        private const string codeHighlighterScript = "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/languages/go.min.js\"></script><link rel=\"stylesheet\" href=\"https://unpkg.com/@highlightjs/cdn-assets@11.2.0/styles/default.min.css\"><script src=\"https://unpkg.com/@highlightjs/cdn-assets@11.2.0/highlight.min.js\"></script><script>hljs.highlightAll();</script>";
+    }
+}
