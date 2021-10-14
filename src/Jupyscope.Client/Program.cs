7f720bfd4955c9e9f2e5177adfc1f358046b0095
Clément Trassoudaine <cltrasso@microsoft.com>
2021-10-14 17:11:35 +0200

    Change html header and footer

--- src/Jupyscope.Client/Program.cs
+++ src/Jupyscope.Client/Program.cs
@@ -1,13 +1,13 @@
+using Jupyscope.Contracts;
+using Jupyscope.Extensions;
+using Jupyscope.Helpers;
+using Jupyscope.Serializers;
+using Jupyscope.TestData;
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-using Jupyscope.Helpers;
-using Jupyscope.Contracts;
-using Jupyscope.Extensions;
-using Jupyscope.Serializers;
-using Jupyscope.TestData;
 
 namespace Telescope.Client
 {
@@ -18,7 +18,7 @@ namespace Telescope.Client
             var notebookJsonString = Encoding.UTF8.GetString(Resources.neural_cue_combination);
             var notebook = TelescopeConverter.Deserialize<Notebook>(notebookJsonString);
             var htmlNotebook = notebook.ToHtml();
-            htmlNotebook = HTMLHeaderHelper.Header + htmlNotebook;
+            htmlNotebook = $"{HTMLHeaderHelper.Header}{htmlNotebook}{HTMLHeaderHelper.Footer}";
             var tempFile = Path.Combine(Path.GetTempPath(), $"temp_{Guid.NewGuid()}.html");
             await File.WriteAllTextAsync(tempFile, htmlNotebook);
             Process.Start(@"cmd.exe ", $@"/c {tempFile}");
--- src/Jupyscope/Helpers/HTMLHeaderHelper.cs
+++ src/Jupyscope/Helpers/HTMLHeaderHelper.cs
@@ -2,7 +2,9 @@
 {
     public static class HTMLHeaderHelper
     {
-        public static readonly string Header = $@"<!DOCTYPE html>{mathFormatterScript}{codeHighlighterScript}";
+        public static readonly string Header = $@"<!DOCTYPE html><html><head>{mathFormatterScript}{codeHighlighterScript}</head><body>";
+
+        public const string Footer = "</body></html>";
 
         private const string mathFormatterScript = "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/katex.min.css\" integrity=\"sha384-zTROYFVGOfTw7JV7KUu8udsvW2fx4lWOsCEDqhBreBwlHI4ioVRtmIvEThzJHGET\" crossorigin=\"anonymous\"><script defer src=\"https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/katex.min.js\" integrity=\"sha384-GxNFqL3r9uRJQhR+47eDxuPoNE7yLftQM8LcxzgS4HT73tp970WS/wV5p8UzCOmb\" crossorigin=\"anonymous\"></script><script defer src=\"https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/contrib/auto-render.min.js\" integrity=\"sha384-vZTG03m+2yp6N6BNi5iM4rW4oIwk5DfcNdFfxkk9ZWpDriOkXX8voJBFrAO7MpVl\" crossorigin=\"anonymous\" onload=\"renderMathInElement(document.body);\"></ script>";
 
